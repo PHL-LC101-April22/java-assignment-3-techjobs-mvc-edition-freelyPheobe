993c3ed6115129645c89dd863ae26dd09a72070c
freelyPheobe <104399179+freelyPheobe@users.noreply.github.com>
2022-09-27 22:40:34 -0400

    Started #3 and its parameters. Need to build out the logic.

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -2,8 +2,7 @@ package org.launchcode.techjobs.mvc.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import static org.launchcode.techjobs.mvc.controllers.ListController.columnChoices;
 
@@ -20,7 +19,15 @@ public class SearchController {
         model.addAttribute("columns", columnChoices);
         return "search";
     }
+//<form th:action="@{/search/results}" method = "post">
+//1.The method should also take in two other parameters, specifying the type of search and the search term.
+//   2. In order for these last two parameters to be properly passed in by Spring Boot,
+//    you need to use the correct annotation. Also, you need to name them appropriately, based on the
+//    corresponding form field names defined in search.html.
+    @PostMapping
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
 
+    }
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
 }
